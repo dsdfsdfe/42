8e185a4f5843b5e1a043abc406cc4d876121d0ac
dsdfsdfe <72729157+dsdfsdfe@users.noreply.github.com>
2020-11-05 01:40:11 +0900

    Add files via upload

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: haoh <haoh@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/02 22:31:40 by haoh              #+#    #+#             */
+/*   Updated: 2020/11/05 00:30:49 by haoh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] > s2[i])
+			return (1);
+		else if (s1[i] < s2[i])
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = 1;
+	while (argc > i + 1)
+	{
+		if (ft_strcmp(argv[i], argv[i + 1]) == 1)
+		{
+			ft_swap(&argv[i], &argv[i + 1]);
+			i = 1;
+		}
+		else
+			i++;
+	}
+	i = 1;
+	while (argc > i)
+	{
+		ft_putstr(argv[i]);
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
