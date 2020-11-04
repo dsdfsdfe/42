14514e8f57dddd4ce63b1445b4dbf85286859ae5
dsdfsdfe <72729157+dsdfsdfe@users.noreply.github.com>
2020-11-05 01:48:07 +0900

    Update ft_strstr.c

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -17,14 +17,16 @@ char	*ft_strstr(char *str, char *to_find)
 
 	i = 0;
 	j = 0;
-	if (to_find[j] != '\0')
+	if (to_find[j] == '\0')
 		return (str);
-	while (str[i] !='\0')
+	while (str[i] != '\0')
 	{
-		while (str[i + j] == to_find[j] && to_find[j] != '\0')
+		while (str[i + j] == to_find[j] && str[i + j] != '\0')
+		{
+			if (to_find[j + 1] == '\0')
+				return (&str[i]);
 			++j;
-		if (to_find[j] == '\0')
-			return (&str[i]);
+		}
 		++i;
 	}
 	return (0);
